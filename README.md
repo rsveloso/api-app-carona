d1882b113ed08b9b362f962fac5704a0fb90c08c
Rafael Veloso <rafael.veloso.ext@gmail.com>
2025-03-01 00:42:59 -0300

    feat: Criando o servidor da nossa aplicação

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-#api-app-carona
+
+Rotas - Projeto EasyCar
+----------------------------------------
+
+
+[X] Consultar se existe corrida pendente (hoje) para o usuario logado
+    GET -> /rides
+    Filtros: passenger_user_id, pickup_date, ride_id, driver_user_id, status
+
+[X] Cadastrar novas caronas
+    POST -> /rides
+
+[X] Cancelar um pedido de carona (excluir o pedido)
+    DELETE -> /rides/123
+
+[x] Finalizar uma carona que foi concluida
+    PUT -> /rides/123/finish
+    Body: passenger_user_id
+
+[X] Consultar as corridas para o motorista (corridas dele + corridas pendentes sem motorista)
+    GET -> /rides/drivers/456
+
+[X] Consultar todos os dados de uma determinada corrida
+    GET -> /rides/123
+
+[X] Motorista aceitar uma carona
+    PUT -> /rides/123/accept
+    Body: driver_user_id  
+
+[X] Motorista cancelar uma carona
+    PUT -> /rides/123/cancel
+
+
+Business Rules (Regras de Negocio)
+----------------------------------------
+
+[ ] O usuario so pode pedir uma carona por vez
+
+[ ] O motorista só pode aceitar uma carona por vez
+
---
+++ src/repositories/repository.ride.js
@@ -0,0 +1,114 @@
+import { execute } from "../database/sqlite.js";
+
+async function List(passenger_user_id, pickup_date, ride_id, driver_user_id, status) {
+
+    let filtro = [];
+
+    let sql = `select r.*, u.name as passenger_name, u.phone as passenger_phone,
+                d.name as driver_name, d.phone as driver_phone
+    from rides r
+    join users u on (u.user_id = r.passenger_user_id)
+    left join users d on (d.user_id = r.driver_user_id)
+    where r.ride_id > 0 `
+
+    if (passenger_user_id) {
+        sql = sql + " and r.passenger_user_id = ? ";
+        filtro.push(passenger_user_id);
+    }
+
+    if (pickup_date) {
+        sql = sql + " and r.pickup_date = ? ";
+        filtro.push(pickup_date);
+    }
+
+    if (ride_id) {
+        sql = sql + " and r.ride_id = ? ";
+        filtro.push(ride_id);
+    }
+
+    if (driver_user_id) {
+        sql = sql + " and r.driver_user_id = ? ";
+        filtro.push(driver_user_id);
+    }
+
+    if (status) {
+        sql = sql + " and r.status = ? ";
+        filtro.push(status);
+    }
+
+    const rides = await execute(sql, filtro);
+    return rides;
+}
+
+async function Insert(passenger_user_id, pickup_address,
+    pickup_latitude, pickup_longitude, dropoff_address) {
+
+    let sql = `insert into rides(passenger_user_id, pickup_address,
+        pickup_latitude, pickup_longitude, dropoff_address, pickup_date, status)
+        values(?, ?, ?, ?, ?, CURRENT_DATE, 'P') returning ride_id`;
+
+    const ride = await execute(sql, [passenger_user_id, pickup_address,
+        pickup_latitude, pickup_longitude, dropoff_address]);
+
+    return ride[0];
+}
+
+async function Delete(ride_id) {
+
+    let sql = `delete from rides where ride_id = ?`;
+
+    await execute(sql, [ride_id]);
+
+    return { ride_id }
+}
+
+async function Finish(ride_id, passenger_user_id) {
+
+    let sql = `update rides set status = 'F' where ride_id = ? and passenger_user_id = ?`;
+
+    await execute(sql, [ride_id, passenger_user_id]);
+
+    return { ride_id }
+}
+
+async function ListForDriver(driver_user_id) {
+
+    let sql = `select r.*, u.name as passenger_name, u.phone as passenger_phone                
+    from rides r
+    join users u on (u.user_id = r.passenger_user_id)
+    where r.pickup_date = CURRENT_DATE
+    and  r.driver_user_id = ? 
+    
+    UNION
+    
+    select r.*, u.name as passenger_name, u.phone as passenger_phone                
+    from rides r
+    join users u on (u.user_id = r.passenger_user_id)
+    where r.pickup_date = CURRENT_DATE
+    and  r.driver_user_id is null 
+    `
+
+
+    const rides = await execute(sql, [driver_user_id]);
+    return rides;
+}
+
+async function Accept(ride_id, driver_user_id) {
+
+    let sql = `update rides set status = 'A', driver_user_id = ? where ride_id = ?`;
+
+    await execute(sql, [driver_user_id, ride_id]);
+
+    return { ride_id }
+}
+
+async function Cancel(ride_id) {
+
+    let sql = `update rides set status = 'P', driver_user_id = null where ride_id = ?`;
+
+    await execute(sql, [ride_id]);
+
+    return { ride_id }
+}
+
+export default { List, Insert, Delete, Finish, ListForDriver, Accept, Cancel };
